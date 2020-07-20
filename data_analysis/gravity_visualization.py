06255fe04ca40a53eafae7949ebef6ee062325de
Peter Herman <peter.r.herman@gmail.com>
2020-07-20 14:55:23 -0400

    Fixed issues with errorbar plot and added new functionality

--- data_analysis/gravity_visualization.py
+++ data_analysis/gravity_visualization.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 from typing import List
 import matplotlib.pyplot as plt
 from scipy.stats import norm
+from numpy import nan
 
 
 
@@ -70,9 +71,11 @@ def gravity_coefficient_error_bars(estimation_model,
                            path:str = None,
                            fig_dimensions:tuple = None,
                            subplot_titles:dict = None,
-                           delete_subplot:list = [],
                            confidence_interval:float = 0.95,
-                           no_xticks:bool = True):
+                           color_significance:float = None,
+                           no_xticks:bool = True,
+                           legend_in_subplot = False,
+                           styles:dict = None):
     '''
     Plot Gravity coefficient estimates from GME with error bars for confidence intervals.
 
@@ -88,22 +91,54 @@ def gravity_coefficient_error_bars(estimation_model,
         delete_subplot: (list[tuple[int, int]]) Optional: Delete specified subplot or plots. Useful if the grid of
             subplots has more cels than are neccessary. For example, if plotting 5 figures in a 3x2 grid, include (0,0)
             to delete the row=0, col=0 cell, leaving a blank space instead of empty axes.
-        confidence_interval: (float) The confidence level to use for the bars. Default is 0.95.
-        no_xticks:  (bool) If True, no x-axis labels are included. Can be useful when sector names are two long to be
+        confidence_interval: (float) The confidence level to use for the bars. Default is 0.95. Use 0 to turn off error
+            bars
+        color_significance: (float) Optional. If supplied, colors insignificant values differently based on significance
+            level. Defualt in None, which does not color estimates differently.
+        no_xticks:  (bool) If True, no x-axis labels are included. Can be useful when sector names are too long to be
             properly displayed olong axis.
+        styles: (dict) Optional. Dictionary to specify certain color and formatting options. Can replace default options
+            by supplying a dictionary containing some or all of the following keys (default values are given). See
+            matplotlib documentation for valid color and format selections.
+                {'est_color':'blue',        # Color of main coefficient estimate points
+                 'est_fmt':'.',             # Format of main estimate points
+                 'est_bar_color':'blue',    # Color of main confidence interval error bar
+                 'insig_color':'red',       # Color of insignificant estimate points (if color_significance specified)
+                 'insig_fmt':'x',           # Format of insignificant estimate points
+                 'insig_bar_color':'red',   # Color of insignificnat error bars
+                 'zero_color':'k',          # Color of zero line
+                 'zero_fmt':'--'}           # Format of zero line
 
     Returns: Produces a plot and, if specified, saves one or more plots at the secified file paths.
 
     '''
     # ---
+    # Prep additional Args
+    # ---
+    style = {'est_color':'C0',
+             'est_fmt':'.',
+             'est_bar_color':'C0',
+             'insig_color':'C3',
+             'insig_fmt':'x',
+             'insig_bar_color':'C3',
+             'zero_color':'k',
+             'zero_fmt':'--'}
+    # Replace default values with user values.
+    if styles is not None:
+        for key, field in styles.items():
+            style[key]=field
+
+
+    # ---
     # Prep Data
     # ---
 
     # Unpack some values
-    sector_var_name = estimation_model.estimation_data.meta_data.sector_var_name
+    sector_var_name = estimation_model.estimation_data._meta_data.sector_var_name
 
     # Collect Estimates and reformat DataFrame to multi-index dataframe
     estimates = estimation_model.combine_sector_results()
+    # ToDo: make this robust to sector names containing '_' (E.g. split on the last '_' or identify specific strings (coeff, pvalue, etc.)
     multi_col_names = [tuple(col.split('_')) for col in estimates.columns]
     multi_index = pd.MultiIndex.from_tuples(multi_col_names, names=['sector', 'param'])
     estimates.columns = multi_index
@@ -114,6 +149,10 @@ def gravity_coefficient_error_bars(estimation_model,
 
     # Reshape estimate type wide
     param_info = param_info.pivot_table(values = 'value',index = [sector_var_name,'variable'], columns='param').reset_index()
+
+    # Get a vector of sectors
+    sector_list = pd.DataFrame({sector_var_name:param_info[sector_var_name].unique()})
+    num_sectors = sector_list.shape[0]
 
     # ---
     # Create Plot
@@ -134,39 +173,102 @@ def gravity_coefficient_error_bars(estimation_model,
 
     # Create Master Plot
     fig, axs = plt.subplots(nrows=n_rows, ncols=n_cols, sharex=True, sharey=False)
-
     # Define coordinates for each subplot
     plot_scheme = [[row, col] for row in range(n_rows) for col in range(n_cols)]
-    if len(plot_scheme) != len(variables):
-        raise ValueError("\n Supplied dimensions ({} subplots) does not match number of variables ({})".format(
-            len(plot_scheme), len(variables)))
-    # Assign a varaible to each sub plot coordinates
-    for num in range(len(plot_scheme)):
+
+    # Check for the right number of subplots
+    if legend_in_subplot:
+        if not len(plot_scheme)>len(variables):
+            raise ValueError("fig_dimensions does not specify enough subplots to include legend in separate plot")
+    if len(plot_scheme)<len(variables):
+        raise ValueError("fig_dimensions does not specify enough subplots for specified variables.")
+
+    #Remove extra plots if necessary
+    if len(plot_scheme)>len(variables):
+        extra_plots = len(plot_scheme)-len(variables)
+        if legend_in_subplot:
+            extra_plots = extra_plots-1
+        for num in range(extra_plots):
+            # Return last value in list
+            del_plot = plot_scheme.pop()
+            fig.delaxes(axs[del_plot[0], del_plot[1]])
+
+    # Add plotting information
+    for num in range(len(variables)):
         plot_scheme[num].append(variables[num])
         plot_scheme[num].append(num)
+    if legend_in_subplot:
+        plot_scheme[-1].append('legend_subplot')
+        plot_scheme[-1].append(len(plot_scheme)-1)
+
 
     # Create each subplot
     for row, col, var_name, num in plot_scheme:
-        var_info = param_info.loc[param_info['variable']==var_name,:].copy()
-        var_info.sort_values(by=[sector_var_name], inplace = True)
         # Grab subplot axis
-        if n_rows <2 or n_cols<2:
+        if n_rows < 2 or n_cols < 2:
             # In cases where there are not multiple rows or columns, 2 dimension coordinates do not work.
             ax = axs[num]
         else:
             ax = axs[row, col]
-        # ax.locator_params(nbins=4)
-        ax.errorbar(var_info[sector_var_name], var_info['coeff'], z_value*var_info['stderr'], fmt = '.', ecolor = 'k')
-        ax.plot(var_info[sector_var_name], [0]*len(var_info[sector_var_name]), linestyle = '--', color = 'orange')
-        ax.set_title(subplot_titles[var_name])
+
+        # If not a legend subplt, create actual coefficient plot
+        if var_name != 'legend_subplot':
+            var_info = param_info.loc[param_info['variable']==var_name,:].copy()
+            if var_info.shape[0]<num_sectors:
+                var_info = sector_list.merge(var_info, how = 'left', on = sector_var_name)
+            var_info.sort_values(by=[sector_var_name], inplace = True)
+
+            # Plot Significant Estimates
+            if color_significance is None:
+                if confidence_interval==0:
+                    coeff_points = ax.errorbar(var_info[sector_var_name], var_info['coeff'], None,
+                                               fmt=style['est_fmt'], ecolor=style['est_bar_color'],
+                                               mec=style['est_color'], mfc=style['est_color'])
+                else:
+                    coeff_points = ax.errorbar(var_info[sector_var_name], var_info['coeff'], z_value*var_info['stderr'],
+                                               fmt = style['est_fmt'], ecolor = style['est_bar_color'],
+                                               mec=style['est_color'], mfc=style['est_color'])
+
+            # Plot significance in different colors
+            if color_significance is not None:
+                insignificant_coeffs = var_info.loc[var_info['pvalue']>(1-color_significance),:]
+                significant_coeffs = var_info.copy()
+                significant_coeffs.loc[significant_coeffs['pvalue']>(1-color_significance),'coeff']=nan
+                if confidence_interval==0:
+                    coeff_points = ax.errorbar(significant_coeffs[sector_var_name], significant_coeffs['coeff'], None,
+                                               fmt=style['est_fmt'], ecolor=style['est_bar_color'],
+                                               mec=style['est_color'], mfc=style['est_color'])
+                    insig_points = ax.errorbar(insignificant_coeffs[sector_var_name], insignificant_coeffs['coeff'],
+                                               None,
+                                               mfc=style['insig_color'], mec=style['insig_color'],
+                                               fmt=style['insig_fmt'], ecolor=style['insig_bar_color'])
+                else:
+                    coeff_points = ax.errorbar(significant_coeffs[sector_var_name], significant_coeffs['coeff'],
+                                               z_value * significant_coeffs['stderr'],
+                                               fmt=style['est_fmt'], ecolor=style['est_bar_color'],
+                                               mec=style['est_color'], mfc=style['est_color'])
+                    insig_points = ax.errorbar(insignificant_coeffs[sector_var_name], insignificant_coeffs['coeff'],
+                                z_value*insignificant_coeffs['stderr'],
+                                               mfc=style['insig_color'], mec=style['insig_color'],
+                                               fmt=style['insig_fmt'], ecolor=style['insig_bar_color'])
+                if not legend_in_subplot:
+                    plt.legend([coeff_points, insig_points],[ "Significant at {} level".format(color_significance),
+                                                          "Not significant at {} level".format(color_significance)])
+
+            ax.plot(var_info[sector_var_name], [0]*len(var_info[sector_var_name]), linestyle = style['zero_fmt'], color = style['zero_color'])
+            ax.set_title(subplot_titles[var_name])
+
+        # If a Legend Subplot, create legend
+        if var_name == 'legend_subplot':
+            ax.set_axis_off()
+            ax.legend([coeff_points, insig_points],[ "Significant at {} level".format(color_significance),
+                                                          "Not significant at {} level".format(color_significance)], loc='center')
+
         if no_xticks:
             plt.setp(ax.get_xticklabels(), visible=False)
 
 
-    # Delete unused plots in grid
-    if len(delete_subplot)!=0:
-        for coord in delete_subplot:
-            fig.delaxes(axs[coord[0],coord[1]])
+
 
     fig.tight_layout(pad=1.0)
     for ax in axs.flat:
